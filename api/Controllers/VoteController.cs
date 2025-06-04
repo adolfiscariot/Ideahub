eea028a42db7b2484554797acf9554a16ed93dfa
adolfiscariot <m10mathenge@gmail.com>
2025-06-04 15:38:40 +0300

    Done with vote controller. Added re-voting functionality

--- api/Controllers/VoteController.cs
+++ api/Controllers/VoteController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using api.Helpers;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace api.Controllers;
 
@@ -64,14 +65,27 @@ public class VoteController : ControllerBase
             }
 
             //check if user has already voted
-            bool hasVoted = _context.Votes.Any(v => v.UserId == userId && v.IdeaId == ideaId);
-            if (hasVoted)
+            var existingVote = await _context.Votes.IgnoreQueryFilters().FirstOrDefaultAsync(v => v.UserId == userId && v.IdeaId == ideaId);
+            if (existingVote != null)
             {
-                _logger.LogError("Cast Vote: User {userName} has already voted for idea {ideaId}", userEmail, ideaId);
-                return StatusCode(403, ApiResponse.Fail("User has already voted"));
+                if (existingVote.IsDeleted == false)
+                {
+                    _logger.LogError("Cast Vote: User {userName} has already voted for idea {ideaId}", userEmail, ideaId);
+                    return StatusCode(403, ApiResponse.Fail("User has already voted"));
+                }
+                else if (existingVote.IsDeleted == true)
+                {
+                    existingVote.IsDeleted = false;
+                    existingVote.DeletedAt = null;
+                    _context.Votes.Update(existingVote);
+                    await _context.SaveChangesAsync();
+
+                    _logger.LogInformation("User {userEmail} has successfully re-voted for idea {ideaId} at {curerntTime}", userEmail, ideaId, DateTime.UtcNow);
+                    return Ok(ApiResponse.Ok("Re-voting successful"));
+                }
             }
 
-            //cast a vote
+            //create a vote
             var vote = new Vote
             {
                 UserId = userId,
@@ -79,8 +93,25 @@ public class VoteController : ControllerBase
             };
 
             //save vote
-            _context.Votes.Add(vote);
-            await _context.SaveChangesAsync();
+            try
+            {
+                vote.IsDeleted = false;
+                _context.Votes.Add(vote);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                if (e.InnerException is PostgresException)
+                {
+                    _logger.LogWarning(e, "Cast Vote: User {userEmail} has already voted for idea {ideaId}", userEmail, ideaId);
+                    return StatusCode(403, ApiResponse.Fail("User has already voted"));
+                }
+                else
+                {
+                    _logger.LogError(e, "Error while casting vote");
+                    return StatusCode(500, ApiResponse.Fail("Error while casting code. Please try again"));
+                }
+            } 
 
             _logger.LogInformation("Cast Vote: Vote cast by {userEmail} for idea {ideaTitle}", userEmail, idea.Title);
             return Ok(ApiResponse.Ok("Vote cast successfully"));
@@ -160,7 +191,7 @@ public class VoteController : ControllerBase
             }
 
             //fetch votes with similar idea id
-            var votes = _context.Votes.Include(v => v.User).Where(v => v.IdeaId == ideaId).ToList();
+            var votes = await _context.Votes.Include(v => v.User).Where(v => v.IdeaId == ideaId).ToListAsync();
             if (votes.Count == 0)
             {
                 _logger.LogWarning("See Votes: Idea {ideaId} has no votes", ideaId);
@@ -169,12 +200,12 @@ public class VoteController : ControllerBase
             var voteDetails = votes.Select(votes => new
             {
                 UserName = votes.User.DisplayName,
-                Email = votes.User.Email,
+                UserEmail = votes.User.Email,
                 Time = votes.VotedAt,
             });
 
             _logger.LogInformation("{voteCount} votes found for idea {ideaId}", votes.Count, ideaId);
-            return Ok(ApiResponse.Ok("Votes found"));
+            return Ok(ApiResponse.Ok($"{votes.Count} Votes found", voteDetails));
         }
         catch (Exception e)
         {
@@ -182,6 +213,4 @@ public class VoteController : ControllerBase
             return StatusCode(500, ApiResponse.Fail("An internal server error prevented all votes from being fetched. Please try again"));
         }
     } 
-    
-    //Count votes (GroupAdminOnly)
 }
