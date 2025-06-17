6fb66d56f14ecaf5b9ddfb66d3a9f3184407f15c
Adolf Iscariot <m10mathenge@gmail.com>
2025-06-17 03:20:09 +0300

    Fixing registration page

--- ui/src/app/Pages/register/register.component.ts
+++ ui/src/app/Pages/register/register.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonsComponent } from '../../Components/buttons/buttons.component';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ButtonsComponent],
   standalone: true,
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
