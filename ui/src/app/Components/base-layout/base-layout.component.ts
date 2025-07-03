c2f7d4b553773075d9d369c27d021f9a1af5bf02
adolfiscariot <m10mathenge@gmail.com>
2025-07-03 18:15:19 +0300

    Sidebar component configured. Styling and polulating left.

--- ui/src/app/Components/base-layout/base-layout.component.ts
+++ ui/src/app/Components/base-layout/base-layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-base-layout',
-  imports: [],
+  imports: [SidebarComponent, RouterOutlet],
   templateUrl: './base-layout.component.html',
   styleUrl: './base-layout.component.scss'
 })
--- ui/src/app/Pages/home/home.component.ts
+++ ui/src/app/Pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BaseLayoutComponent } from "../../Components/base-layout/base-layout.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BaseLayoutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
