7f769b110543a21244aa8f7a50c4965c2db00b33
adolfiscariot <m10mathenge@gmail.com>
2026-01-12 11:12:30 +0300

    fix: add new migration

---
+++ api/Migrations/20260112080902_IsGroupPublic.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ideahub.Migrations
+{
+    public partial class IsGroupPublic : Migration 
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Groups",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true); // set default for existing rows
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Groups");
+        }
+    }
+}
