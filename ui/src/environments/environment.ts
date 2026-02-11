cdbf9b14e680aec8d520795266edcf5e5a9d39a9
adolfiscariot <m10mathenge@gmail.com>
2026-02-11 15:22:58 +0300

    fix: env file api url

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 // environment.ts (dev)
 export const environment = {
   apiUrl: 'http://localhost:5065/api'
-=======
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:5065'
->>>>>>> dev
 };
