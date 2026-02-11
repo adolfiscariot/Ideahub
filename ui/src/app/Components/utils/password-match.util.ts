96b4a3c45f18de252ccbaf8ca2395548759b476b
JoyMicheni <micheni73@gmail.com>
2026-02-11 13:01:55 +0300

    feat: create password match checker util

---
+++ ui/src/app/Components/utils/password-match.util.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function passwordMatchValidator(
+  control: AbstractControl
+): ValidationErrors | null {
+  const password = control.get('newPassword')?.value;
+  const confirm = control.get('confirmPassword')?.value;
+
+  return password === confirm ? null : { passwordMismatch: true };
+}
