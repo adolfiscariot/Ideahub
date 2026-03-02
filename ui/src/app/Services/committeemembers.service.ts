bab13e8648f635eec894b4760232fe4d2dd5c64e
adolfiscariot <m10mathenge@gmail.com>
2026-03-02 18:59:53 +0300

    feat: added committee members functionality

--- ui/src/app/Services/committeemembers.service.ts
+++ ui/src/app/Services/committeemembers.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { ApiResponse } from '../Interfaces/Api-Response/api-response';
 
 @Injectable({
