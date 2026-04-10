0a0c145926817132dc1f0c7079a8829c69e2800b
JoyMicheni <micheni73@gmail.com>
2026-04-10 11:35:45 +0300

    refactor: switch from staging server to live domain

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://44.211.117.129:5065'
+  apiUrl: 'https://ideahub.adept-techno.co.ke'
 };
