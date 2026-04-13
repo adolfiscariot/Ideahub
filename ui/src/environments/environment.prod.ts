d88aa40f89ae33288475d395cad28a5852327346
adolfiscariot <164077251+adolfiscariot@users.noreply.github.com>
2026-04-13 10:32:19 +0300

    Update apiUrl to include '/api' in production

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://ideahub.adept-techno.co.ke'
+  apiUrl: 'https://ideahub.adept-techno.co.ke/api'
 };
