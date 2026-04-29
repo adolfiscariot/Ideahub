54618736709b930ffaeba5f16323d8447a605cb3
JoyMicheni <micheni73@gmail.com>
2026-04-29 16:14:38 +0300

    feat: change current auth guard to auth0 guard, configure auth0 for the frontend handles authentication, refreshtokens

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,33 +1,50 @@
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
-  APP_INITIALIZER,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
-import { authInterceptor } from './Interceptors/auth/auth.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { heroBell } from '@ng-icons/heroicons/outline';
 import { provideIcons } from '@ng-icons/core';
-import { AppConfigService } from './core/services/app-config.service';
+import { provideAuth0, AuthHttpInterceptor } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment';
 
-function initConfig(cfg: AppConfigService) {
-  return () => cfg.load();
-}
+// function initConfig(cfg: AppConfigService) {
+//   return () => cfg.load();
+// }
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(withInterceptors([authInterceptor])),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initConfig,
-      deps: [AppConfigService],
-      multi: true,
-    },
-    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
     provideIcons({ heroBell }),
+    provideAuth0({
+      domain: 'dev-2685h5q7efjt6peh.us.auth0.com',
+      clientId: '04mFGQUmAGjOE1t18D8r0drPuPHUXEWO',
+      useRefreshTokens: true,
+      cacheLocation: 'localstorage',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        audience: 'https://api.ideahub',
+        scope: 'openid profile email offline_access',
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: `${environment.apiUrl}/*`,
+            tokenOptions: {
+              authorizationParams: {
+                audience: 'https://api.ideahub',
+                scope: 'openid profile email offline_access',
+              }
+            }
+          }
+        ]
+      }
+    }),
   ],
 };
